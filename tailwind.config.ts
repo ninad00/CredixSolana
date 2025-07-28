916561dc4f015b933d5068e30a286e0eb0b85fde
abhishekmane1911 <abhishekmane1911@gmail.com>
2025-07-29 00:25:41 +0530

    fixed css errors

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
